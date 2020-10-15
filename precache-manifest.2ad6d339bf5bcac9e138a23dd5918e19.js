668032cb05846c481fec74ea9e686f61a9279f91
irenhh <iriskag@ukr.net>
2020-10-16 01:42:08 +0300

    Updates

---
+++ precache-manifest.2ad6d339bf5bcac9e138a23dd5918e19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527fdc4e6dda40cd98e02718dbf2b7e7",
+    "url": "/irenhh/waverly-javascript/index.html"
+  },
+  {
+    "revision": "1562ca3aeec12c345983",
+    "url": "/irenhh/waverly-javascript/static/css/main.8195615c.chunk.css"
+  },
+  {
+    "revision": "3ff5d834a972e95da026",
+    "url": "/irenhh/waverly-javascript/static/js/2.fbb55251.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/irenhh/waverly-javascript/static/js/2.fbb55251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1562ca3aeec12c345983",
+    "url": "/irenhh/waverly-javascript/static/js/main.1cd3bf45.chunk.js"
+  },
+  {
+    "revision": "690b1976bbdffeb42744",
+    "url": "/irenhh/waverly-javascript/static/js/runtime-main.a5f25b99.js"
+  }
+]);
